818222a6bd49287dff8a5691550b7b2442d00b86
3cvvh <ayane9012@gmail.com>
2025-08-18 21:42:44 +0700

    ashley so cute

---
+++ pertemuan-2(popup box)/sc.js
@@ -0,0 +1,7 @@
+var umur = prompt('berapa umur anda?:');
+var k = confirm('apakah anda yakin?');
+if(k == true){
+    alert('umur anda adalah: ' + umur );
+}else{
+    alert('umur mu tidak ada')
+}
---
+++ ulang/script.js
@@ -0,0 +1,8 @@
+alert('selamat datang');
+var lagi = true;
+while(lagi == true){
+var nama = prompt('masukan nama anda:');
+alert('nama anda adalah '+ nama);
+lagi = confirm('ingin main lagi?');
+}
+alert('terimakasih')
